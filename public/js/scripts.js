b639a4f6874357589161223dca83b1c1fd8b6c52
MrRojito-Dev <cancinojuaco@gmail.com>
2021-07-15 12:12:10 -0400

    First app version

---
+++ public/js/scripts.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+	document.getElementById("info").innerText = `Cargando...`;
+	const fragment = new URLSearchParams(window.location.hash.slice(1));
+	const [accessToken, tokenType] = [
+		fragment.get("access_token"),
+		fragment.get("token_type"),
+	];
+
+	if (!accessToken) {
+		document.getElementById("info").innerText =
+			"Bienvenido, inicia sesión para continuar:";
+
+		return (document.getElementById("login").style.display = "block");
+	}
+
+	fetch("https://discord.com/api/users/@me", {
+		headers: {
+			authorization: `${tokenType} ${accessToken}`,
+		},
+	})
+	.then((result) => result.json())
+	.then((response) => {
+		const user = response;
+		console.log(user);
+		document.getElementById("info").innerText = `Bienvenido, ${user.username}#${user.discriminator}`;
+	})
+	.catch(console.error);
+
+};
