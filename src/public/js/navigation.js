9ca51a9eeee10bad1c5f81f8c972e288dde441df
MrRojito-Dev <cancinojuaco@gmail.com>
2021-07-27 20:04:31 -0400

    + animaciones

--- src/public/js/navigation.js
+++ src/public/js/navigation.js
@@ -11,12 +11,15 @@ window.onload = () => {
                 navIconMenu.classList.add("fa-angle-down")
                 navIconMenu.classList.remove("open");
 
+                navMenu.classList.add("animate__bounceOutUp")
                 navMenu.classList.remove("open");  
             } else {
                 navIconMenu.classList.remove("fa-angle-down")
                 navIconMenu.classList.add("fa-angle-up");
                 navIconMenu.classList.add("open");
                 
+                navMenu.classList.remove("animate__bounceOutUp");
+                navMenu.classList.add("animate__bounceInDown");
                 navMenu.classList.add("open");  
             }
         };
